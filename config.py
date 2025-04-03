d9af34c31ab3d08683d11e234526e42572078c27
Vladimir Kokkinaki <ehmatwan@gmail.com>
2025-04-03 14:26:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
-BOT_TOKEN = 'your-telegram-bot-token'
-OPENAI_API_KEY = 'your-openai-key'
-TOYYIB_SECRET_KEY = 'your-toyyibpay-secret-key'
-TOYYIB_CATEGORY_CODE = 'your-category-code'
-TOYYIB_USER_EMAIL = 'youremail@example.com'
-QR_ACCOUNT_INFO = 'Scan QR and pay to:\nAhmad Hazwan\nHSBC 013-253752-108'
-QR_IMAGE_URL = '/static/qrpay.png'  # You can replace with your real QR image
+BOT_TOKEN = '8116658927:AAGLhlnwhwdyRHrQxr8_sNz98LicNtI1rwA'
+OPENAI_API_KEY = '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'
+TOYYIB_SECRET_KEY = 'l0f6cyv6-tv47-hdlz-xqas-to1o6j1amh6v'
+TOYYIB_CATEGORY_CODE = 'gnuga7m4'
+TOYYIB_USER_EMAIL = 'ehmatwan@gmail.com'
+QR_ACCOUNT_INFO = 'Scan QR and pay to:\nAhmad Hazwan\nHSBC 013253752108'
+QR_IMAGE_URL = 'https://i.ibb.co/MkvHDZWk/b2d3933a-87a7-4fd6-a18d-cbe880223d6a.jpg'  # You can replace with your real QR image
